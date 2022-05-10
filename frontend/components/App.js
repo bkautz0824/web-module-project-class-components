983d3e33aea415d23e050a6aceaae0cf033fd940
Bennett Kautz <bennettkautz@gmail.com>
2022-05-10 11:17:38 -0400

    gettting App and Todolist to work

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
+
+const todos = [
+  {
+    task: "walk the dog",
+    id: 1,
+    finsished: false
+  },
+  {
+    task: "take out trash",
+    id: 2,
+    finsished: false
+  },
+  {
+    task: "workout",
+    id: 3,
+    finsished: false
+  },
+
+]
+
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: todos,
+    }
+  }
+
+
+
   render() {
+
     return (
       <div>
-        Todo App
-        console.log("")
+        <TodoList  
+          todos={this.state.todos}
+        />
+       <Form  />
       </div>
     )
   }
